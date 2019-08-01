c0e461b3bf340f56bd955288c44a71907fb53e75
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-08-01 20:09:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require "pry"
 def nyc_pigeon_organizer(data)
   binding.pry
+data.each {|key,value|
   male_names = data[:gender][:male].collect
   female_names = data[:gender][:female].collect
 end
